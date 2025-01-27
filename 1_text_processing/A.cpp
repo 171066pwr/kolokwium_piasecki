7290399681d1b79b976abce05de82168b4cd7bd4
171066pwr <171066@student.pwr.edu.pl>
2025-01-27 22:24:24 +0100

    1_A poprawione

--- 1_text_processing/A.cpp
+++ 1_text_processing/A.cpp
@@ -1,42 +1,15 @@
 /*Przetwarzanie tekstów -  ³añcuchów znaków
      (tablica zawieraj¹ca przetwarzany tekst powinna byæ parametrem
        wejœciowo/wyjœciowym pisanej funkcji)
-
-
-
- b)	Napisz funkcjê, która usunie wszystkie pocz¹tkowe spacje
-    w ³añcuchu tekst, który powinien byæ parametrem wejœciowo/wyjœciowym tej funkcji.
-
- c)	Napisz funkcjê formatuj¹c¹ tekst poprzez usuniêcie
-    wszystkich pocz¹tkowych i koñcowych spacji
-    z ³añcucha zadanego jako parametr wejœciowy tej funkcji.
-
- d)	Napisz funkcjê usuwaj¹c¹ (poprzez skrócenie tekstu)
-    wszystkie spacje z ³añcucha podawanego jako parametr tej funkcji.
-
- e)	Napisz funkcjê usuwaj¹c¹ z zadanego ³añcucha (poprzez skrócenie tekstu)
-    wszystkie bezpoœrednie powtórzenia liter np. zamieniaj¹c¹ tekst “kommputter” na ”komputer”.
-
- f)	Napisz funkcjê dodaj¹c¹ zadan¹ iloœæ spacji
-    na pocz¹tku zadanego ³añcucha tekst
-    (wczeœniejsza zawartoœæ tekstu przesuwana jest na dalsze pozycje).
-
- g)	Napisz funkcjê usuwaj¹c¹ z ³añcucha tekst wszystkie litery
-    poczynaj¹c od pozycji pocz do litery na pozycji kon.
-
- h)	Napisz funkcjê wyrównuj¹c¹ d³ugoœæ zadanego tekst'u do N znaków,
-    poprzez dodanie odpowiedniej iloœci spacji na koñcu tego ³añcucha.
-
- i)	Napisz funkcjê sprawdzaj¹c¹ czy zadany ³añcuch znaków
-    ma symetryczn¹ zawartoœæ tzn, pierwszy znak jest równy ostatniemu,
-    drugi przedostatniemu itd.
-    */
+*/
+// a)	Napisz program, który wczyta ³añcuch znaków z klawiatury
+//    	a nastêpnie usunie z tego ³añcucha pierwsz¹ ma³¹ literê.
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-const int limit = 10;
+const int limit = 100;
 
 void scan(char *array, int size) {
 	fgets(array, size, stdin);
@@ -85,18 +58,13 @@ void zadAMemmove(char *array, int size) {
 	}	
 }
 
-// a)	Napisz program, który wczyta ³añcuch znaków z klawiatury
-//    	a nastêpnie usunie z tego ³añcucha pierwsz¹ ma³¹ literê.
 int main() {
 	char tab[limit];
-	char tab2[limit];
+	scan(tab, limit);
 
 	printf("%s\n", tab);
-	zadA(tab, 10);
+	zadA(tab, limit);
 	printf("%s\n", tab);
-	
-	zadAMemmove(tab2, 10);
-	printf("%s\n", tab2);
 
 	system("pause");
 }
